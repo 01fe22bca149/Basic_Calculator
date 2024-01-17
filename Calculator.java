06f48ab9764bef725f62c762719be9880020cc6c
Suraj B <01fe22bca149@kletech.ac.in>
2024-01-17 17:45:48 +0530

    updated of square functionality

--- Calculator.java
+++ Calculator.java
@@ -3,8 +3,7 @@
  * The Calculator class is used for performing mathematical calculations.
  */
 /**
- * The Calculator class provides methods for performing addition, subtraction, multiplication, and
- * division operations on two operands.
+ 
  */
 class Calculator
 {
@@ -20,6 +19,9 @@ class Calculator
     float division( float operand_1,float operand_2){
         return(operand_1/operand_2);
     } 
+    float square(float operand_1){
+        return(operand_1*operand_1);
+    }
     public static void main(String[] args) {
         Calculator calc= new Calculator();
         float operand_1=10;
@@ -36,6 +38,9 @@ class Calculator
 
     float div_result=calc.division(operand_1,operand_2);
     System.out.println("result is:"+div_result);
+
+    float squ_result=calc.square(operand_1,operand_2);
+    System.out.println("result is:"+squ_result);
     }
         
     }
